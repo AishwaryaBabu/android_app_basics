fc764c11b17451c7263afa481ccf354eb592eb7c
Aishwarya Babu <babu.aishwarya@gmail.com>
2013-04-11 02:10:54 +0200

    1) Added pop up menu in the contacts at the options button. 2) Profile Activity: Archive button changes to another color when pressed

--- src/com/example/app1/Contacts.java
+++ src/com/example/app1/Contacts.java
@@ -5,28 +5,66 @@ import java.util.ArrayList;
 import com.example.app1.data.ContactsDatabase;
 
 import android.app.Activity;
-import android.content.Intent;
+//import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.Toast;
 
 
-public class Contacts extends Activity{
-	
+public class Contacts extends Activity implements OnClickListener{
+
 	private ListView list;
-	
+	private Button optionsButton;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.contacts);
-	    list=(ListView)findViewById(R.id.list);
-	    ContactsDatabase helper=new ContactsDatabase(Contacts.this);   
-	      
-	    Intent intent = getIntent();
-	    ArrayList<String> items=helper.GetNames();
-	    ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
-	    list.setAdapter(adapter);
-	    helper.close();
+		list=(ListView)findViewById(R.id.list);
+		ContactsDatabase helper=new ContactsDatabase(Contacts.this);   
+
+		//	    Intent intent = getIntent();
+		ArrayList<String> items=helper.GetNames();
+		ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+		list.setAdapter(adapter);
+		helper.close();
+		
+		optionsButton = (Button) findViewById(R.id.button1);
+
+		
+		optionsButton.setOnClickListener(this);	
+
 	}
+
+	public void onClick(View v) {
+	/** Instantiating PopupMenu class */
+	PopupMenu popup = new PopupMenu(getBaseContext(), v);
+
+	/** Adding menu items to the popupmenu */
+	popup.getMenuInflater().inflate(R.menu.contact_popup, popup.getMenu());
+
+	/** Defining menu item click listener for the popup menu */
+	popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+		@Override
+		public boolean onMenuItemClick(MenuItem item) {
+			Toast.makeText(getBaseContext(), "You selected the action : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+			return true;
+		}
+	});
+
+	/** Showing the popup menu */
+	popup.show();
+	}
+
+
 }
--- src/com/example/app1/ProfileActivity.java
+++ src/com/example/app1/ProfileActivity.java
@@ -4,25 +4,31 @@ package com.example.app1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+//import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+//import android.widget.PopupMenu;
+//import android.widget.Toast;
+//import android.widget.PopupMenu.OnMenuItemClickListener;
 
 //public class ProfileActivity {
 public class ProfileActivity extends Activity implements OnClickListener {
 
 	private Button archiveButton;
 	private ImageView profilePic;
-	
+
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
 		
 		archiveButton = (Button) findViewById(R.id.archive_button);
+
 		profilePic = (ImageView) findViewById(R.id.profile_pic);
 		
 		archiveButton.setOnClickListener(this);
+		
 	}
 
 	@Override
@@ -30,6 +36,8 @@ public class ProfileActivity extends Activity implements OnClickListener {
 		Intent intent1= new Intent(this, ArchiveActivity.class);
 		startActivity(intent1);
 		// TODO Auto-generated method stub		
+		
+		
 	}
 
 }
